c4a14cebd835c933a6d020cdc249a3d82d913b2c
Kirit Sælensminde <k@kirit.com>
2015-05-11 22:52:47 +0700

    Create an example executable.

---
+++ examples/events/main.cpp
@@ -0,0 +1,15 @@
+/*
+    Copyright 2015, Proteus Tech Co Ltd. http://www.kirit.com/Rask
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/main>
+
+
+FSL_MAIN("fsnotify-events", "fsnotify event display")
+        (fostlib::ostream &out, fostlib::arguments &args) {
+    return 0;
+}
