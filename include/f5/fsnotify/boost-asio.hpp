574b0ba25b0ade27b91384c10822f9dad8b5446c
Kirit Sælensminde <k@kirit.com>
2015-05-27 12:10:36 +0700

    Initial empty skeleton for the reader.

---
+++ include/f5/fsnotify/boost-asio.hpp
@@ -0,0 +1,36 @@
+/*
+    Copyright 2015, Proteus Tech Co Ltd. http://www.kirit.com/Rask
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#pragma once
+
+
+namespace f5 {
+
+
+    inline namespace fsnotify {
+
+
+        namespace boost_asio {
+
+
+            struct reader {
+                /// Callback that kicks off a ASIO task
+                template<typename N>
+                void loop(N &notifier, int fd) {
+                };
+            };
+
+
+        }
+
+
+    }
+
+
+}
+
